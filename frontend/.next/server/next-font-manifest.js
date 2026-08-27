34a1ab248f56c7006116a2f939f443dd712a530c
Amama Aziz <amamaazizchishtie@gmail.com>
2026-08-27 17:08:21 +0500

    Stop tracking .next build folder

--- frontend/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{\"/_app\":[\"static/media/f3f7e95f2dbc4fe4-s.p.woff2\",\"static/media/e4af272ccee01ff0-s.p.woff2\",\"static/media/bb3ef058b751a6ad-s.p.woff2\"]},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":true}"
